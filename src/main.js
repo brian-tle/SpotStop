695af18625af900a27b610938414c9ed333c4af8
William Lew <WilliamMilesLew@gmail.com>
2019-03-02 13:44:30 -0800

    removed streetview

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+var mapOptions = {
+	streetViewControl: false
+};
+
 function initMap() {
-  // The location of Uluru
-  var uluru = {lat: 37.7219, lng: -122.4782};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 12, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+	// The location of Uluru
+	var uluru = {lat: 37.7219, lng: -122.4782};
+	// The map, centered at Uluru
+	var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: uluru});
+	map.setOptions(mapOptions);
+	// The marker, positioned at Uluru
+	var marker = new google.maps.Marker({position: uluru, map: map});
 }
