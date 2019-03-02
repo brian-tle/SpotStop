61f5edeec07700c85c14f003bc7317714ce9a4bc
ShotaEbikawa <shotaebikawa@gmail.com>
2019-03-02 13:09:04 -0800

    changed the default location

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
+  var uluru = {lat: 37.773972, lng: -122.431297};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {zoom: 12, center: uluru});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
