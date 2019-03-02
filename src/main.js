c314b2302e0c4b182ff6eafc069470d13213793b
Brian Le <brianle8910@gmail.com>
2019-03-02 15:35:40 -0800

    Added Clickable Marker
    - Implement drop marker on location
    - Cursor still there while active
    - esc / cancel to close

--- src/main.js
+++ src/main.js
@@ -1,6 +1,42 @@
 var mapOptions = {
-	streetViewControl: false
+streetViewControl: false,
+fullscreenControl: false
 };
+
+function CenterControl(controlDiv, map) {
+    // Set CSS for the control border.
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '22px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Propose Marker?';
+    controlDiv.appendChild(controlUI);
+    
+    // Set CSS for the control interior.
+    var controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.marginRight = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.style.MarginTop = '5px';
+    controlText.innerHTML = 'Propose Marker';
+    controlUI.appendChild(controlText);
+    
+    // Setup the click event listeners: simply set the map to Chicago.
+    controlUI.addEventListener('click', function() {
+                               map.setOptions({
+                                              draggableCursor:'url(res/icons/marker_red.png), auto'});
+                               
+                               
+                               console.log("Button works!");
+                               });
+}
 
 function initMap() {
 	// The location of Uluru
@@ -9,7 +45,15 @@ function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: uluru});
 	map.setOptions(mapOptions);
 	// The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+    
+    var centerControlDiv = document.createElement('div');
+    var centerControl = new CenterControl(centerControlDiv, map);
+    
+    centerControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(centerControlDiv);
+    
+    
+    var marker = new google.maps.Marker({position: uluru, map: map});
 	indicator = new MarkerIndicator(marker.getPosition());
 	indicator.init(map);
 }
