95cfdf5a0fb0e99d08b9503e132849b117fff70d
Brian Le <brianle8910@gmail.com>
2019-03-02 19:03:24 -0800

    Cleaning map_contr

--- src/map_control.js
+++ src/map_control.js
@@ -1,4 +1,7 @@
 var cursorRunning = false;
+var pasted = false;
+
+elementToChange.style.cursor = 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur%22), auto';
 
 function CenterControl(controlDiv, map) {
     // Set CSS for the control border.
@@ -27,19 +30,19 @@ function CenterControl(controlDiv, map) {
     
     // Setup the click event listeners: transform cursor to red
     controlUI.addEventListener('click', function() {
-        cursorRunning = true;
-        map.setOptions({ draggableCursor:'url(res/icons/marker_red.png), auto' });
-   });
+                               cursorRunning = true;
+                               map.setOptions({ draggableCursor:'url(res/icons/marker_red.png), auto' });
+                               });
 }
 
 function addListenerControl(map) {
     map.addListener('click', function(event) {
-        if (cursorRunning) {
-            marker = new google.maps.Marker({position: event.latLng, map: map});
-            map.setOptions({ draggableCursor:'url("https://maps.gstatic.com/mapfiles/openhand_8_8.cur%22), auto' });
-            cursorRunning = false;
-        }
-    });
+                    if (cursorRunning) {
+                    marker = new google.maps.Marker({position: event.latLng, map: map});
+                    map.setOptions({ draggableCursor:'url("https://maps.gstatic.com/mapfiles/openhand_8_8.cur%22), default' });
+                    cursorRunning = false;
+                    }
+                    });
 }
 
 function addControls(map) {
