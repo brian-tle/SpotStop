2de9d421e7e0bd94f03ae8bb980c4c3e06e52619
ShotaEbikawa <shotaebikawa@gmail.com>
2019-03-02 19:01:26 -0800

    created marker class

--- src/Markers.js
+++ src/Markers.js
@@ -1,22 +1,28 @@
-class Markers{
-    constructor(){
+class Marker{
+
+    constructor(x,y){
+		this.lat = x;
+		this.lng = y;
+		this.dict = {lat: parseFloat(x), lng: parseFloat(y)};
+
 	}
-
-
-	 createmarker(x,y) {
-		// The location of Uluru
-	   
-		var uluru = {lat: x, lng: y};
+	 //createmarker(x,y) {
+		
 		// The map, centered at Uluru
-		var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: uluru});
-		map.setOptions(mapOptions);
+	//	var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: this.m});
+	//	map.setOptions(mapOptions);
 		// The marker, positioned at Uluru
-		var marker = new google.maps.Marker({position: uluru, map: map,animation:google.maps.Animation.DROP});
+	//	var marker = new google.maps.Marker({position: this.m, map: map,animation:google.maps.Animation.DROP});
+
 		
-		
+	//}
+	get m() {
+		return this.mm();
 	}
-	
 			
+	mm() {
+		return this.dict;
+	}
     
 	
 }
