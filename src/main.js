6c4db04432c99e085fc0fa47ec45ea42ee09c946
ShotaEbikawa <shotaebikawa@gmail.com>
2019-03-02 20:47:29 -0800

    added markers

--- src/main.js
+++ src/main.js
@@ -1,28 +1,25 @@
 var mapOptions = {
     streetViewControl: false,
     fullscreenControl: false
-};
-
-function initMap() {
+ };
+ 
+ function initMap() {
     const m = new Marker(37.7219, -122.4782);
     var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: m.m});
     map.setOptions(mapOptions);
     // The marker, positioned at Uluru
-    
+   
     addControls(map)
     addListeners(map)
-
-
+ 
     m.createMarker(map);
     var marker = m.GetMarker;
     indicator = new MarkerIndicator(marker.getPosition());
-
-}
-
-function addListeners(map) {
-    map.addListener('zoom_changed', function(event) {
-        indicator.handleZoom(map.getZoom());
-    });
-
+    indicator.init(map);
+    indicator.addListeners();
+ }
+ 
+ function addListeners(map) {
     addListenerControl(map)
-}
+ }
+ 
