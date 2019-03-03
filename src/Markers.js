d001f1ba207cf848e1892a8111a87f91ccc38e18
Jason Wei <jason.wei.1106@gmail.com>
2019-03-02 16:21:39 -0800

    update createmarker

--- src/Markers.js
+++ src/Markers.js
@@ -1,5 +1,12 @@
-class proposeMarker {
-    function CreateClass() {
-
-    };    
+function createmarker(x,y) {
+	// The location of Uluru
+   
+    var uluru = {lat: x, lng: y};
+	// The map, centered at Uluru
+	var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: uluru});
+	map.setOptions(mapOptions);
+	// The marker, positioned at Uluru
+	var marker = new google.maps.Marker({position: uluru, map: map});
+	
+    
 }
--- src/main.js
+++ src/main.js
@@ -3,11 +3,5 @@ var mapOptions = {
 };
 
 function initMap() {
-	// The location of Uluru
-	var uluru = {lat: 37.7219, lng: -122.4782};
-	// The map, centered at Uluru
-	var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: uluru});
-	map.setOptions(mapOptions);
-	// The marker, positioned at Uluru
-	var marker = new google.maps.Marker({position: uluru, map: map});
+	createmarker( 37.7219, -122.4782)
 }
