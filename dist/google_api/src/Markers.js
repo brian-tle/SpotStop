f8aa478c0459e93f65e66cd04498cbb219b2a283
Brian Le <brianle8910@gmail.com>
2019-03-03 04:19:42 -0800

    :space_invader: :heart: :beers:

--- dist/google_api/src/Markers.js
+++ dist/google_api/src/Markers.js
@@ -1,6 +1,8 @@
-const ICON_COLOR_RED = 'ff0000';
-const ICON_COLOR_GREEN = '51c944';
-const ICON_COLOR_GRAY =  '808080';
+const ICON_COLOR_RED = 'ff0000';    //BAD
+const ICON_COLOR_ORANGE = 'ef9423'; //EH
+const ICON_COLOR_GRAY =  '808080'; //NOOTRAL
+const ICON_COLOR_GREEN = '78bc71';  //mellou
+const ICON_COLOR_DARK_GREEN = '0d9e00'; //WOO
 
 class Marker {
   constructor(x, y, totalPoints = 30) {
@@ -23,9 +25,17 @@ class Marker {
   }
 
   createMarker(map) {
-      if (this.totalPoints > 45) { this.color = ICON_COLOR_GREEN; } 
-      else if (this.totalPoints < 15) { this.color = ICON_COLOR_RED; } 
-      else {this.color = ICON_COLOR_GRAY; }
+      if ( this.totalPoints > 45) {
+          this.color = ICON_COLOR_DARK_GREEN;
+      } else if (this.totalPoints <= 45 && this.totalPoints > 37) {
+          this.color = ICON_COLOR_GREEN;
+      } else if (this.totalPoints <= 37 && this.totalPoints > 23) {
+          this.color = ICON_COLOR_GRAY;
+      } else if (this.totalPoints <= 23 && this.totalPoints > 15) {
+          this.color = ICON_COLOR_ORANGE;
+      } else if (this.totalPoints < 15) {
+          this.color = ICON_COLOR_RED;
+      }
       
       this.image = 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2238%22%20height%3D%2238%22%20viewBox%3D%220%200%2038%2038%22%3E%3Cpath%20fill%3D%22%23' + this.color + '%22%20stroke%3D%22%23ccc%22%20stroke-width%3D%22.5%22%20d%3D%22M34.305%2016.234c0%208.83-15.148%2019.158-15.148%2019.158S3.507%2025.065%203.507%2016.1c0-8.505%206.894-14.304%2015.4-14.304%208.504%200%2015.398%205.933%2015.398%2014.438z%22%2F%3E%3Ctext%20transform%3D%22translate%2819%2018.5%29%22%20fill%3D%22%23fff%22%20style%3D%22font-family%3A%20Arial%2C%20sans-serif%3Bfont-weight%3Abold%3Btext-align%3Acenter%3B%22%20font-size%3D%2212%22%20text-anchor%3D%22middle%22%3E' + this.totalPoints + '%3C%2Ftext%3E%3C%2Fsvg%3E';
      
@@ -79,9 +89,17 @@ class Marker {
   }
 
   refreshIcon() {
-      if (this.totalPoints > 45) { this.color = ICON_COLOR_GREEN; } 
-      else if (this.totalPoints < 15) { this.color = ICON_COLOR_RED; } 
-      else {this.color = ICON_COLOR_GRAY; }
+      if ( this.totalPoints > 45) {
+          this.color = ICON_COLOR_DARK_GREEN;
+      } else if (this.totalPoints <= 45 && this.totalPoints > 37) {
+          this.color = ICON_COLOR_GREEN;
+      } else if (this.totalPoints <= 37 && this.totalPoints > 23) {
+          this.color = ICON_COLOR_GRAY;
+      } else if (this.totalPoints <= 23 && this.totalPoints > 15) {
+          this.color = ICON_COLOR_ORANGE;
+      } else if (this.totalPoints < 15) {
+          this.color = ICON_COLOR_RED;
+      }
       
       this.image = 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2238%22%20height%3D%2238%22%20viewBox%3D%220%200%2038%2038%22%3E%3Cpath%20fill%3D%22%23' + this.color + '%22%20stroke%3D%22%23ccc%22%20stroke-width%3D%22.5%22%20d%3D%22M34.305%2016.234c0%208.83-15.148%2019.158-15.148%2019.158S3.507%2025.065%203.507%2016.1c0-8.505%206.894-14.304%2015.4-14.304%208.504%200%2015.398%205.933%2015.398%2014.438z%22%2F%3E%3Ctext%20transform%3D%22translate%2819%2018.5%29%22%20fill%3D%22%23fff%22%20style%3D%22font-family%3A%20Arial%2C%20sans-serif%3Bfont-weight%3Abold%3Btext-align%3Acenter%3B%22%20font-size%3D%2212%22%20text-anchor%3D%22middle%22%3E' + this.totalPoints + '%3C%2Ftext%3E%3C%2Fsvg%3E';
       this.marker.setIcon(this.image);
