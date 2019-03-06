dd7fae70389081b4c7730fe4323e433edcdeb1aa
William Lew <WilliamMilesLew@gmail.com>
2019-03-06 13:36:01 -0800

    added marker size scale depending on the rating

--- dist/google_api/src/markers.js
+++ dist/google_api/src/markers.js
@@ -13,6 +13,8 @@ class Marker {
     this.default = { lat: parseFloat(x), lng: parseFloat(y) };
     this.marker;
 
+    this.size = 40;
+
     this.positionOffset = { lat: parseFloat(x + 0.0005), lng: parseFloat(y) };
     if (!existing) {
       createMarker(x, y, "New Node", 0, 0);
@@ -30,24 +32,32 @@ class Marker {
   createMarker(map) {
       if ( this.totalPoints > 45) {
           this.color = ICON_COLOR_DARK_GREEN;
+          this.size = 60;
       } else if (this.totalPoints <= 45 && this.totalPoints > 37) {
           this.color = ICON_COLOR_GREEN;
+          this.size = 50;
       } else if (this.totalPoints <= 37 && this.totalPoints > 23) {
           this.color = ICON_COLOR_GRAY;
+          this.size = 40;
       } else if (this.totalPoints <= 23 && this.totalPoints > 15) {
           this.color = ICON_COLOR_ORANGE;
+          this.size = 30;
       } else if (this.totalPoints < 15) {
           this.color = ICON_COLOR_RED;
+          this.size = 20;
       }
-      
-      this.image = 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2238%22%20height%3D%2238%22%20viewBox%3D%220%200%2038%2038%22%3E%3Cpath%20fill%3D%22%23' + this.color + '%22%20stroke%3D%22%23ccc%22%20stroke-width%3D%22.5%22%20d%3D%22M34.305%2016.234c0%208.83-15.148%2019.158-15.148%2019.158S3.507%2025.065%203.507%2016.1c0-8.505%206.894-14.304%2015.4-14.304%208.504%200%2015.398%205.933%2015.398%2014.438z%22%2F%3E%3Ctext%20transform%3D%22translate%2819%2018.5%29%22%20fill%3D%22%23fff%22%20style%3D%22font-family%3A%20Arial%2C%20sans-serif%3Bfont-weight%3Abold%3Btext-align%3Acenter%3B%22%20font-size%3D%2212%22%20text-anchor%3D%22middle%22%3E' + this.totalPoints + '%3C%2Ftext%3E%3C%2Fsvg%3E';
+
+    this.icon = {
+        url: 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2238%22%20height%3D%2238%22%20viewBox%3D%220%200%2038%2038%22%3E%3Cpath%20fill%3D%22%23' + this.color + '%22%20stroke%3D%22%23ccc%22%20stroke-width%3D%22.5%22%20d%3D%22M34.305%2016.234c0%208.83-15.148%2019.158-15.148%2019.158S3.507%2025.065%203.507%2016.1c0-8.505%206.894-14.304%2015.4-14.304%208.504%200%2015.398%205.933%2015.398%2014.438z%22%2F%3E%3Ctext%20transform%3D%22translate%2819%2018.5%29%22%20fill%3D%22%23fff%22%20style%3D%22font-family%3A%20Arial%2C%20sans-serif%3Bfont-weight%3Abold%3Btext-align%3Acenter%3B%22%20font-size%3D%2212%22%20text-anchor%3D%22middle%22%3E' + this.totalPoints + '%3C%2Ftext%3E%3C%2Fsvg%3E',
+        scaledSize: { width: this.size, height: this.size }
+    };
      
     this.map = map;
     this.marker = new google.maps.Marker({
       position: this.default,
       animation: google.maps.Animation.DROP,
       map: map,
-      icon: this.image
+      icon: this.icon
     });
 
     this.marker.addListener('click', function(event) {
@@ -94,18 +104,27 @@ class Marker {
   refreshIcon() {
       if ( this.totalPoints > 45) {
           this.color = ICON_COLOR_DARK_GREEN;
+          this.size = 60;
       } else if (this.totalPoints <= 45 && this.totalPoints > 37) {
           this.color = ICON_COLOR_GREEN;
+          this.size = 50;
       } else if (this.totalPoints <= 37 && this.totalPoints > 23) {
           this.color = ICON_COLOR_GRAY;
+          this.size = 40;
       } else if (this.totalPoints <= 23 && this.totalPoints > 15) {
           this.color = ICON_COLOR_ORANGE;
+          this.size = 30;
       } else if (this.totalPoints < 15) {
           this.color = ICON_COLOR_RED;
+          this.size = 20;
       }
+
+      this.icon = {
+          url: 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2238%22%20height%3D%2238%22%20viewBox%3D%220%200%2038%2038%22%3E%3Cpath%20fill%3D%22%23' + this.color + '%22%20stroke%3D%22%23ccc%22%20stroke-width%3D%22.5%22%20d%3D%22M34.305%2016.234c0%208.83-15.148%2019.158-15.148%2019.158S3.507%2025.065%203.507%2016.1c0-8.505%206.894-14.304%2015.4-14.304%208.504%200%2015.398%205.933%2015.398%2014.438z%22%2F%3E%3Ctext%20transform%3D%22translate%2819%2018.5%29%22%20fill%3D%22%23fff%22%20style%3D%22font-family%3A%20Arial%2C%20sans-serif%3Bfont-weight%3Abold%3Btext-align%3Acenter%3B%22%20font-size%3D%2212%22%20text-anchor%3D%22middle%22%3E' + this.totalPoints + '%3C%2Ftext%3E%3C%2Fsvg%3E',
+          scaledSize: { width: this.size, height: this.size }
+      };
       
-      this.image = 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2238%22%20height%3D%2238%22%20viewBox%3D%220%200%2038%2038%22%3E%3Cpath%20fill%3D%22%23' + this.color + '%22%20stroke%3D%22%23ccc%22%20stroke-width%3D%22.5%22%20d%3D%22M34.305%2016.234c0%208.83-15.148%2019.158-15.148%2019.158S3.507%2025.065%203.507%2016.1c0-8.505%206.894-14.304%2015.4-14.304%208.504%200%2015.398%205.933%2015.398%2014.438z%22%2F%3E%3Ctext%20transform%3D%22translate%2819%2018.5%29%22%20fill%3D%22%23fff%22%20style%3D%22font-family%3A%20Arial%2C%20sans-serif%3Bfont-weight%3Abold%3Btext-align%3Acenter%3B%22%20font-size%3D%2212%22%20text-anchor%3D%22middle%22%3E' + this.totalPoints + '%3C%2Ftext%3E%3C%2Fsvg%3E';
-      this.marker.setIcon(this.image);
+      this.marker.setIcon(this.icon);
   }
 
   get getMarker() {
