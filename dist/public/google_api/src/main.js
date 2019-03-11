a05d1ae3a43634b4f083f58875c1be19b5022f1d
ShotaEbikawa <shotaebikawa@gmail.com>
2019-03-11 16:37:04 -0700

    smartphone compatible

--- dist/public/google_api/src/main.js
+++ dist/public/google_api/src/main.js
@@ -35,7 +35,7 @@ function initAutocomplete() {
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
-  
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(input);
   
 
   // Bias the SearchBox results towards current map's viewport.
