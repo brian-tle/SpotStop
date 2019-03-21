e0f4df2f1b29434c822a96cca1820324b41aa69a
ShotaEbikawa <shotaebikawa@gmail.com>
2019-03-21 13:48:10 -0700

    pushed changes in user.js

--- dist/public/userbase/user.js
+++ dist/public/userbase/user.js
@@ -32,16 +32,20 @@ class User {
         return this.rating;
     }
 }
+var user_data;
 
 function createUser() {
     var user = document.getElementById("username").value;
     var pass = document.getElementById("password").value;
     var re_pass = document.getElementById("repeat_password").value;
+    var check = 0;
+
     if (pass == re_pass) {
         window.alert('Signed Up!!!');
         addUser(user, pass);
     }
+
     else {
-        window.alert("Password is incorrect!!!")
+        window.alert("Password is incorrect!!!");
     }
 }
