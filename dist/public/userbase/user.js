3ecf43089c687ed21f87a942b43125c7a80cf003
ShotaEbikawa <shotaebikawa@gmail.com>
2019-03-24 03:48:32 -0700

    added comments

--- dist/public/userbase/user.js
+++ dist/public/userbase/user.js
@@ -47,9 +47,15 @@ function createUser() {
     var email = document.getElementById("email").value;
     var pass = document.getElementById("password").value;
     var re_pass = document.getElementById("repeat_password").value;
+    // to check whether username and/or email exists
+    // in the database
     var user_check = 0;
     var email_check = 0;
     
+    // checks the database if username and/or
+    // email already exists
+    // if username exist, user_check == 1
+    // if email exists, email_check == 1
     for (var key in user_data) {
         if (user_data.hasOwnProperty(key)) {
             if (user_data[key].username == user) {
@@ -61,16 +67,20 @@ function createUser() {
         }
     }
     
+    // lets user know that username already exists
     if ((pass == re_pass) && user_check > 0) {
         window.alert('Username already exists!');
         event.preventDefault();
     }
 
+    // lets user know that email already exists
     else if ((pass == re_pass) && email_check > 0) {
         window.alert('Email address already exists!');
         event.preventDefault();
     }
     
+    // adds user if all the condition satisfies
+    // redirects the user back to the homepage
     else if ((pass == re_pass) && user_check == 0 && email_check == 0) {
         addUser(user, email, pass);
         window.alert('Signed Up!!!');
@@ -79,6 +89,8 @@ function createUser() {
  
     } 
 
+    // lets user know that the password and
+    // the repeated password weren't matching
     else {
         window.alert("Password is incorrect!!!");
         event.preventDefault();
